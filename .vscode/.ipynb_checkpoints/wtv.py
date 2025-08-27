c1775b4c9ff18ea507675cb43a631c0ce62cf02f
LeenA-hub <almutileen7@gmail.com>
2025-08-26 23:27:51 -0400

    index

--- .vscode/.ipynb_checkpoints/wtv.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(os.path.exists("data"))            # Should print True
-print(os.path.exists("data/items.csv"))  # Should print True if file is correct
-print(os.listdir("data"))                # Lists all files inside data
